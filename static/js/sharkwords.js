d76630ef59ba9f0dc1eb28ed951895d265f682cc
G Ayodele <grace.dele@gmail.com>
2022-11-21 16:11:04 -0500

    finished first part of lab. minor changes to for functions to avoid having to access the index. Few functions added from second part of lab as well

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,25 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const letter of word){
+  const container = document.querySelector('#word-container')
+  container.insertAdjacentHTML(
+    'beforeend',
+    `<div class="letter-box ${letter}"></div>`);
+  }
 };
+
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
   // Replace this with your code
+  for (const letter of ALPHABET){
+    const container = document.querySelector('#letter-buttons')
+    container.insertAdjacentHTML(
+      'beforeend',
+      `<button class="letter-buttons">${letter}</button>`);
+    }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -35,6 +47,7 @@ const generateLetterButtons = () => {
 //
 const disableLetterButton = (buttonEl) => {
   // Replace this with your code
+  buttonEl.disabled = true;
 };
 
 // This is a helper function we will use in the future
@@ -43,6 +56,33 @@ const disableLetterButton = (buttonEl) => {
 
 const isLetterInWord = (letter) => {
   // Replace this with your code
+  const letterin = document.querySelector(letter);
+  if (letterin == null){
+    return false;}
+  else{
+    return true;
+  }
+};
+
+const handleCorrectGuess = (letter) => {
+  // Replace this with your code
+};
+
+//
+// Called when `letter` is not in word.
+//
+// Increment `numWrong` and update the shark image.
+// If the shark gets the person (5 wrong guesses), disable
+// all buttons and show the "play again" message.
+
+const handleWrongGuess = () => {
+  numWrong += 1;
+  // Replace this with your code
+};
+
+//  Reset game state. Called before restarting the game.
+const resetGame = () => {
+  window.location = '/sharkwords';
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -57,10 +97,17 @@ const isLetterInWord = (letter) => {
 
   // call the function that makes an empty line for each letter in the word
   // Replace this line with the function call
+  createDivsForChars('apple');
 
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
+  generateLetterButtons();
 
-  // in the next lab, you will be adding functionality to handle when
-  // someone clicks on a letter
+  for (const button of document.querySelectorAll('button')) {
+    // add an event handler to handle clicking on a letter button
+    // YOUR CODE HERE
+  }
+
+  // add an event handler to handle clicking on the Play Again button
+  // YOUR CODE HERE
 })();
